29b1d53ede5de76c7bc8434955c1a9a40ea6ddf0
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2022-10-08 21:18:35 +0600

    verify token correction

--- middleware/verifytoken.js
+++ middleware/verifytoken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) return next(errorResponse("You are not authenticated!", 401));
 
-  jwt.verify(token, process.env.JWT, (err, user) => {
+  jwt.verify(token, process.env.JWT_PASS, (err, user) => {
     if (err) return next(errorResponse("Token is not valid!"), 403);
     req.user = user;
     next();
